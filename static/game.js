50174c7aa95167074a4f75083a5552aaeb33ef6e
Sleeeeeepy <adjustedmin@gmail.com>
2023-10-12 18:07:07 +0900

    feat: 소숫점 둘 째 자리까지 서버에 전송

--- static/game.js
+++ static/game.js
@@ -400,7 +400,7 @@ export function start() {
         let lengthX = Math.abs(x2 - x1);
         let lengthY = Math.abs(y2 - y1);
         let score = Math.min(lengthX / lengthY, lengthY / lengthX) * 100;
-
+        score = parseFloat(score.toFixed(2));
         return score;
     }
 
